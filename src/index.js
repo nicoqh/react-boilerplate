a89fcf56aa5f477af24512ab1cfe8c80263a91d2
nicoqh <nicoqh@gmail.com>
2019-06-04 00:05:05 +0200

    import react, create and render our first component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-const greet = name => console.log(`Hello, ${name}`);
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-greet('Jon Snow');
+function Root() {
+  return <h1>Hello, world.</h1>;
+}
+
+// Render the Root element into the DOM
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root'),
+);
