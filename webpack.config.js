59704e4fcf516d76c3d2da3d8db64acbfd3a616e
nicoqh <nicoqh@gmail.com>
2022-03-19 13:19:26 +0100

    configure webpack to use babel-loader
    Babel configs are now placed in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -35,4 +35,44 @@ module.exports = {
     // https://webpack.js.org/configuration/output/#outputpublicpath
     publicPath: "/",
   },
+
+  // Determine how the different types of modules will be treated.
+  // https://webpack.js.org/configuration/module
+  // https://webpack.js.org/concepts#loaders
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Apply this rule to files ending in .js
+        exclude: /node_modules/, // Don't apply to files residing in node_modules
+        use: {
+          // Use the following loader and options
+          loader: "babel-loader",
+          // We can pass options to both babel-loader and Babel. This option object
+          // will replace babel.config.js
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  debug: true, // Output the targets/plugins used when compiling
+
+                  // Configure how @babel/preset-env handles polyfills from core-js.
+                  // https://babeljs.io/docs/en/babel-preset-env
+                  useBuiltIns: "usage",
+
+                  // Specify the core-js version. Must match the version in package.json
+                  corejs: 3,
+
+                  // Specify which environments we support/target for our project.
+                  // (We have chosen to specify targets in .browserslistrc, so there
+                  // is no need to do it here.)
+                  // targets: "",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
