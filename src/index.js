fc30a881090911bf28a53bda7bdd3cdb83165aad
nicoqh <nicoqh@gmail.com>
2022-03-19 13:54:45 +0100

    import react, create and render our first component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-const greet = (name) => console.log(`Hello, ${name}`);
+import React from "react";
+import ReactDOM from "react-dom";
 
-greet("Jon Snow");
+function Root() {
+  return <h1>Hello, world.</h1>;
+}
+
+// Render the Root element into the DOM
+ReactDOM.render(<Root />, document.getElementById("root"));
+
+// If you're using React 18, you can use the new root API:
+// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
